d51380e3d842ee9c6978d6e56cca60b8c084c13d
Sinan Goo <sinangoo@artcom.de>
2014-02-06 22:29:42 +0100

    working on image dragging

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,38 @@ angular.module('FallAgainApp')
       'Karma'
     ];
 
+    $scope.x = 0;
+    $scope.y = 0;
+    $scope.down = 0;
+    $scope.event = '';
+
     $scope.say = function(text) {
       console.log(text);
     };
 
-    $scope.x = 0;
-    $scope.y = 0;
-    $scope.down = 0;
+    $scope.test = function(event,down) {
+      //console.log('test');
+      //var element = angular.element( document.querySelector( '#test' ) );
+      //console.log('test');
+      //console.log('e: ' + JSON.stringify(event));
+      //console.log('e: ' + JSON.stringify(event.which));
+      console.log('e: ' + down);
+      if (down === 1) {
+        console.log('test');
+        var element = document.getElementById('image');
+        //console.log(JSON.stringify(element));
+        //element.innerHTML='hoho';
+        element.style.marginTop = event.pageY + 'px';
+        element.style.marginLeft = event.pageX + 'px';
+        //console.log('x ' + $scope.x);
+        //console.log('y ' + $scope.y);
+        //console.log('x ' + event.pageX);
+        //console.log('y ' + event.pageY);
+        //console.log(JSON.stringify(element.style));
+        //element.text = 'hhhhhhhh';
+      }
+
+    };
+
 
   });
